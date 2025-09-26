a9895380ab8f2686b346974cda44d4e24af739d3
tomiconicgit <tomiconicx@icloud.com>
2025-09-26 14:07:10 +0100

    Update engine.js

--- src/core/engine.js
+++ src/core/engine.js
@@ -1,41 +1,25 @@
 // file: src/core/engine.js
 
 export default class Engine {
-    constructor() {
-        console.log("Engine constructor called.");
-    }
-
     /**
-     * This method is called by the loader *after* validation.
-     * It defines the precise assets needed to render the initial game state.
-     * In the future, this will check a save file. For now, it returns a default state.
+     * This is the manifest. The loader will ask for this list.
+     * We are only asking it to load our test script.
      */
-    getInitialRequiredAssets() {
-        console.log("Engine is providing the initial asset list to the loader.");
-
-        // This is where you define the default starting state.
-        const initialState = {
-            scene: 'desert_outpost',
-            playerPosition: [0, 0, 0],
-            playerEquipment: ['basic_gear']
-        };
-
-        // Based on the state, return the list of assets to load.
+    getManifest() {
         return [
-            { name: 'Player Model',       type: 'model',  path: './assets/models/player_basic_gear.glb' },
-            { name: 'Desert Scene',       type: 'model',  path: './assets/models/desert_outpost.glb' },
-            { name: 'Sand Texture',       type: 'texture',path: './assets/textures/sand.ktx2' },
-            { name: 'Player HUD',         type: 'script', path: './src/ui/playerhud.js' }
+            { name: 'Test Screen', type: 'script', path: './src/core/test.js' }
         ];
     }
 
-    async init() {
-        console.log("Engine.init() called.");
-        // Set up the scene, renderer, and player using the assets that have just been loaded.
+    /**
+     * These methods will be used later when the actual game is running.
+     * For now, they can be empty.
+     */
+    async init() { 
+        // Not used in this test
     }
 
     start() {
-        console.log("Engine.start() called. Beginning render loop.");
-        // Start the main render loop.
+        // Not used in this test
     }
 }
