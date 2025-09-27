e2ca492eaf6ff49e60dec750878c4a77f3cb4a38
tomiconicgit <tomiconicx@icloud.com>
2025-09-27 17:09:44 +0100

    Create three.js

---
+++ src/core/three.js
@@ -0,0 +1,7 @@
+// file: src/core/three.js
+import * as THREE from 'three';
+
+/**
+ * Creates and exports the single, global THREE.Scene instance for the game.
+ */
+export const scene = new THREE.Scene();
