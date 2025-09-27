2ce60f05c3eb1385449d4793c3f8fc4537aa2721
tomiconicgit <tomiconicx@icloud.com>
2025-09-27 17:54:33 +0100

    Update lighting.js

--- src/core/lighting.js
+++ src/core/lighting.js
@@ -1,25 +1,31 @@
 // file: src/core/lighting.js
 import * as THREE from 'three';
+import { scene } from './three.js';
 
 export default class Lighting {
+  static main = null;
+
+  static create() {
+    if (Lighting.main) {
+      console.warn('Lighting already created.');
+      return;
+    }
+    Lighting.main = new Lighting(scene);
+  }
+
   constructor(scene) {
-    // Hemisphere light (soft ambient)
     this.hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 2);
     this.hemiLight.color.setHSL(0.6, 1, 0.6);
     this.hemiLight.groundColor.setHSL(0.095, 1, 0.75);
     this.hemiLight.position.set(0, 50, 0);
     scene.add(this.hemiLight);
 
-    // Directional "sun" light + shadows
     this.dirLight = new THREE.DirectionalLight(0xffffff, 3);
     this.dirLight.color.setHSL(0.1, 1, 0.95);
     this.dirLight.position.set(-1, 1.75, 1).multiplyScalar(30);
     this.dirLight.castShadow = true;
-
-    // Shadow quality / coverage
     this.dirLight.shadow.mapSize.width = 4096;
     this.dirLight.shadow.mapSize.height = 4096;
-
     const d = 400;
     this.dirLight.shadow.camera.left = -d;
     this.dirLight.shadow.camera.right = d;
@@ -27,7 +33,6 @@ export default class Lighting {
     this.dirLight.shadow.camera.bottom = -d;
     this.dirLight.shadow.camera.far = 3500;
     this.dirLight.shadow.bias = -0.0001;
-
     scene.add(this.dirLight);
   }
 }
